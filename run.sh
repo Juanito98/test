5b9f98f71073a80568e1f5cefe35eb17d0f2972e
Juan Carlos Sigler Priego <juan@repl.it>
2024-01-04 17:53:36 -0600

    test

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
 docker run \
-    -v ~/.gitconfig:/root/.gitconfig:ro \
-    -v ~/.ssh:/root/.ssh \
-    -v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} -e SSH_AUTH_SOCK \
-    -v "$(pwd)":/usr/src/app \
-    -e COPYBARA_OPTIONS="--force" \
-    -it olivr/copybara copybara
-
-
-docker run \
-    -v ~/.gitconfig:/root/.gitconfig:ro \
+    -v ~/.gitconfig:/root/.gitconfig \
     -v "$(pwd)":/usr/src/app \
     -e COPYBARA_OPTIONS="--force" \
     -it olivr/copybara copybara
